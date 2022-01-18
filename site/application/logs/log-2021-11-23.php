ac3b0d8619bb151bcb664cebd8b575a4350fa941
harold <s.lefebvre907@laposte.net>
2022-01-18 14:09:58 +0100

    village green

---
+++ site/application/logs/log-2021-11-23.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Src/bg-image.png
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:20 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:21 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:40 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:41 --> 404 Page Not Found: Assets/src
+ERROR - 2021-11-23 10:37:48 --> 404 Page Not Found: Assets/src
